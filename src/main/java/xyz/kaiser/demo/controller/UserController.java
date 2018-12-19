5923c6c4b93168602d3395fde7eab777391f0d19
caddyz <18784227116@163.com>
2018-12-20 01:18:46 +0800

    增加一个

--- src/main/java/xyz/kaiser/demo/controller/UserController.java
+++ src/main/java/xyz/kaiser/demo/controller/UserController.java
@@ -1,9 +1,14 @@
 package xyz.kaiser.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.kaiser.demo.pojo.User;
+import xyz.kaiser.demo.service.IUserService;
 
 /**
  * <p>
@@ -16,5 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    @Autowired
+    private IUserService us;
+    @GetMapping("/find")
+    public User find(@RequestParam("uid") Integer userId){
+        return us.getById(userId);
+    }
 }
